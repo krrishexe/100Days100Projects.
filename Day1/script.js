26c6ae5c19bcba207137199e2dbc2917be08b5bf
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-10-31 08:12:08 -0700

    image slider done

---
+++ Day1/script.js
@@ -0,0 +1,32 @@
+let flag = 2
+
+
+function handleOnClick(x){
+    flag = flag + x;
+    slideShow(flag)
+}
+
+function slideShow(num){
+    let slides = document.getElementsByClassName("slide")
+
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }
+    if(num < 0){
+        flag = slides.length -1
+        num = slides.length -1 
+    }
+
+    for(let y of slides){
+        y.style.display = "none";
+    }
+
+    slides[num].style.display = "block";
+}
+slideShow(flag)
+
+
+
+
+
