1530eb30ce9e7c1e594dc1adc79205224e9556bf
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-03 13:42:28 +0530

    uppercase btn done

---
+++ Day4 - TextUtils/script.js
@@ -0,0 +1,17 @@
+const textArea = document.getElementById("floatingTextarea2");
+
+// console.log(textArea.value);
+
+textArea.addEventListener('input', function handleChange(event) {
+    console.log(event.target.value);
+    let text = event.target.value;
+});
+
+
+
+function upperCase(text){
+    text = textArea.value.toUpperCase()
+    textArea.value = text;
+    console.log(text);
+}
+upperCase();
