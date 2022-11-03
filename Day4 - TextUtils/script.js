34dd39ca45005a6a32e7fc920c67519786d03e35
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-03 13:47:53 +0530

    lowecase btn also done

--- Day4 - TextUtils/script.js
+++ Day4 - TextUtils/script.js
@@ -15,3 +15,17 @@ function upperCase(text){
     console.log(text);
 }
 upperCase();
+
+function lowerCase(text){
+    text = textArea.value.toLowerCase()
+    textArea.value = text;
+    console.log(text);
+}
+lowerCase();
+
+function clearBtn(text){
+    text = textArea.value.toUpperCase()
+    textArea.value = text;
+    console.log(text);
+}
+clearBtn();
