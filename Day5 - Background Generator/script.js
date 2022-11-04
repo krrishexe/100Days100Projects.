a787ea76f75957b049d51cf24e3d20d36359c67e
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-04 12:49:06 +0530

    changed linear gradient

--- Day5 - Background Generator/script.js
+++ Day5 - Background Generator/script.js
@@ -1,1 +1,10 @@
-document.getElementById('gradient')
+var body = document.getElementById('gradient')
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
+
+function bgChanger(){
+    console.log("object");
+    body.style.background = `linear-gradient(to right,${color1.value} ,${color2.value} )`;
+}
+color1.addEventListener("input",bgChanger)
+color2.addEventListener("input",bgChanger)
