d03a06d843c3cf1f6f7817cfccfbb279c1779b78
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-07 16:48:41 +0530

    added javascript

--- Day8 - Random Quoute Generator/script.js
+++ Day8 - Random Quoute Generator/script.js
@@ -1,12 +1,35 @@
 const randomDiv = document.getElementById("random")
 const btnquoteDiv = document.getElementById("btnquote")
 
+console.log("object");
+                                                // 2 Methods.
+
+// method - 1
 
 function run(){
-    fetch("https://zenquotes.io/api/random")
-    .then(Response => Response.json())
+    fetch("https://api.quotable.io/random")
+    .then(response => response.json())
     .then(json => {
-        randomDiv.innerText = `${json.message}`
-        console.log(json.message);
+        console.log(typeof(json));
+        randomDiv.innerText = `Quote of the Day is : ${json.content}`
     })
+    console.log(response.json());
 }
+
+
+// method - 2
+
+// const run = async() => {
+//     const url = 'https://api.quotable.io/random';
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(typeof(data));
+//     randomDiv.innerText = `quote of the day is ${data.content}`
+
+//     console.log(data);
+// }   
+
+btnquoteDiv.onclick =()=> run();
+
+console.log("hello");
+
