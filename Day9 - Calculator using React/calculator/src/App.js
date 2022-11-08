6c146b7ca033aa2e6b808615a4dddcccc9557a30
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-08 11:31:23 +0530

    css done

--- Day9 - Calculator using React/calculator/src/App.js
+++ Day9 - Calculator using React/calculator/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import './media/style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+
+        <div className="output">
+          <div className="previousOutput">1234,444</div>
+          <div className="currentOutput">123456</div>
+        </div>
+
+        <button className='span-two'>AC</button>    
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button className='span-two'>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
     </div>
   );
 }
