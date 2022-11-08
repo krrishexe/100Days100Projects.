d44c2ee59cc420f43b1fc2901bd69b4dd0af6912
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-08 16:07:52 +0530

    added onclick and showonscreen

--- Day9 - Calculator using React/calculator/src/App.js
+++ Day9 - Calculator using React/calculator/src/App.js
@@ -1,34 +1,68 @@
+import { useState } from 'react';
 import './App.css';
 import './media/style.css';
 
+
+
+
 function App() {
+  
+  const showOnScreen =(buttonValue) =>{
+    setText(buttonValue);
+    console.log(buttonValue);
+    console.log(setText(`{buttonValue}`));
+
+    const currentOutputDiv = document.getElementById("previous");
+    currentOutputDiv.innerHTML = `<p>${buttonValue}</p>`
+    
+  }
+
+  const handleOnClick =(event)=>{
+    // console.log(text);
+    const buttonValue = event.target.value;
+
+    console.log(event.target.value);
+    
+
+    showOnScreen(buttonValue);
+    }
+
+
+  const handleOnClear =()=>{
+    // console.log(text);
+    setText("")
+    }
+
+  const [text,setText] = useState("0")
+
   return (
     <div className="App">
       <div className="grid-container">
 
         <div className="output">
-          <div className="previousOutput">1234,444</div>
-          <div className="currentOutput">123456</div>
+          <div id ="previous" className="previousOutput"><p>0</p></div>
+          <div className="currentOutput"><p>0</p></div>
         </div>
 
-        <button className='span-two'>AC</button>    
-        <button>DEL</button>
-        <button>/</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button className='span-two'>0</button>
-        <button>.</button>
-        <button>=</button>
+        <button onClick={handleOnClear} className='span-two'>AC</button>    
+        <button onClick={handleOnClick}>DEL</button>
+        <button value={`/`} onClick={handleOnClick}>/</button>
+        <button value={1} onClick={handleOnClick}>1</button>
+        <button value={2} onClick={handleOnClick}>2</button>
+        <button value={3} onClick={handleOnClick}>3</button>
+        <button value={`*`} onClick={handleOnClick}>*</button>
+        <button value={4} onClick={handleOnClick}>4</button>
+        <button value={5} onClick={handleOnClick}>5</button>
+        <button value={6} onClick={handleOnClick}>6</button>
+        <button value={`+`} onClick={handleOnClick}>+</button>
+        <button value={7} onClick={handleOnClick}>7</button>
+        <button value={8} onClick={handleOnClick}>8</button>
+        <button value={9} onClick={handleOnClick}>9</button>
+        <button value={`-`} onClick={handleOnClick}>-</button>
+        <button value={0} onClick={handleOnClick} className='span-two'>0</button>
+        <button value={`.`} onClick={handleOnClick}>.</button>
+        <button value={`=`} onClick={handleOnClick}>=</button>
+
       </div>
     </div>
   );
