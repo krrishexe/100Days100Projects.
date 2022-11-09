70335acd1a6d21b5ec77d46178389c3e97ffb15f
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-09 23:26:54 +0530

    javascript logic done

--- Day10 - Tip Calculator/script.js
+++ Day10 - Tip Calculator/script.js
@@ -2,10 +2,33 @@ const billTotalInputDiv = document.getElementById('billTotalInput')
 const tipInputDiv= document.getElementById('tipInput')
 const numberOfPeopleDiv = document.getElementById('numberOfPeople')
 const perPersonTotalDiv = document.getElementById('perPersonTotal')
+const people = document.getElementById('people')
 
-let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+let numberOfPeople = Number(numberOfPeopleDiv.innerText);
 
 const calculateBill =()=>{
+
+    const bill = Number(billTotalInputDiv.value);
+
+        // get the tip from user & convert it into a percentage (divide by 100)
+
+    let tipPercent = Number(tipInputDiv.value / 100);
+
+        // get the total tip amount
+
+    let tipAmount = tipPercent * bill;
+
+        // calculate the total (tip amount + bill)
+
+    const total = tipAmount + bill ;
+
+        // calculate the per person total (total divided by number of people)
+
+    const perPersonTotal = total / numberOfPeople;
+
+        // update the perPersonTotal on DOM & show it to user
+
+    perPersonTotalDiv.innerText=`$${perPersonTotal}`
 
 }
 
@@ -18,11 +41,11 @@ const increasePeople=()=>{
 }
 const decreasePeople = () =>{
     if(numberOfPeople<=1){
-        return 
+        return ;
     }
     else{
         numberOfPeople-=1;
-        numberOfPeopleDiv.innerText =`${numberOfPeople}`
+        numberOfPeopleDiv.innerText = `${numberOfPeople}`
         calculateBill();
     }
 }
