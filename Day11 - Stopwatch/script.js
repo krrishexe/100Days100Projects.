4b31f93d1e735412af6994b379f1753d1b7866c7
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-10 19:09:25 +0530

    css done , js logic building

---
+++ Day11 - Stopwatch/script.js
@@ -0,0 +1,8 @@
+const minutesDiv = document.getElementById('minutes');
+const secondsDiv = document.getElementById('seconds');
+const tensDiv = document.getElementById('tens');
+
+const start = document.getElementById('startbtn')
+const stop = document.getElementById('stopbtn')
+const reset = document.getElementById('resetbtn')
+
