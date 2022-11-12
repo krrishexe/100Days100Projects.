1c2d5783a5d64e325194e42228743cc5c8fc31ee
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-12 21:19:56 +0530

    updated js and centered img

--- Day13 - QR Code Generator/script.js
+++ Day13 - QR Code Generator/script.js
@@ -1,14 +1,15 @@
 function generateQRCode(){
-    let website = document.getElementById('website');
+    let website = document.getElementById('website').value;
     if(website){
         let qrCodeImage = document.getElementById('qrcode');
         qrCodeImage.innerHTML = "";
         new QRCode(qrCodeImage,website);
+        
 
         let container = document.getElementById('qrcode-container')
         container.style.display = "block";
     }
     else{
-        alert("Please Enter a Valid URL")
+        alert("Please Enter a Valid URL");
     }
 }
