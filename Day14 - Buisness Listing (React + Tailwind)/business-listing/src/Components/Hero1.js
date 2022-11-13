4e7df69d674639b17df364feb8cc96ce79d1fde0
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-14 01:11:46 +0530

    updated css

--- Day14 - Buisness Listing (React + Tailwind)/business-listing/src/Components/Hero1.js
+++ Day14 - Buisness Listing (React + Tailwind)/business-listing/src/Components/Hero1.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function Hero1(props) {
   return (
     <div>
-      <div className="bg-white font-bold font-apo text-6xl py-40">
+      <div className="bg-white text-6xl py-40">
         <h1>
-          <span>always be</span>
-          <div class="message">
-            <div class="word1">close</div>
-            <div class="word2">code</div>
-            <div class="word3">Creative</div>
+          <span>Business </span>
+          <div className="message font-rale">
+            <div className="word1 ">Startup</div>
+            <div className="word2 ">Idea's</div>
+            <div className="word3 ">Creative</div>
           </div>
         </h1>
       </div>
