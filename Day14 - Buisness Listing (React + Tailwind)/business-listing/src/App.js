c3d887c977a1b9abf620f0e8f4a6357bfc65c940
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-13 22:36:11 +0530

    updated hero section

--- Day14 - Buisness Listing (React + Tailwind)/business-listing/src/App.js
+++ Day14 - Buisness Listing (React + Tailwind)/business-listing/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css"
 import Navbar from "./Components/Navbar";
 import hero from "./media/images/73810-business-idea-animation.gif"
 
@@ -9,11 +10,21 @@ function App() {
 
       <Navbar />
 
-      <div className=" flex justify-end">
-        <img src={hero} alt=""/>
+
+      <div className="bg-white font-bold font-apo text-6xl py-40 ">
+        <span className="tracking-widest">Buisness Listing </span>
       </div>
 
-    
+      <div className="relative h-100 heroBg">
+        <div className="flex justify-end my-5"> 
+          <img src={hero} alt=""/>
+        </div>
+        </div>
+
+        <div></div>
+      
+
+
 
 
 
