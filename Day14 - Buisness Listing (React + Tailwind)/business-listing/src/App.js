99ad6ebfee6e7166f36d5fc6ac207edf31ca494b
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-13 19:06:50 +0530

    react init

--- Day14 - Buisness Listing (React + Tailwind)/business-listing/src/App.js
+++ Day14 - Buisness Listing (React + Tailwind)/business-listing/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div class="w-9/12 mx-auto py-5 flex justify-between items-center">
+            <div class="text-lg">
+              <a href="#">Business Listing</a>
+            </div>
+
+            <div>
+              <button class="button py-2 px-4 rounded-md border border-amber-500 bg-transparent hover:text-white">
+                Add your Business
+              </button>
+            </div>
+
+            <div>
+              <button></button>
+              <button></button>
+            </div>
+
+        </div>
+      </nav>
     </div>
   );
 }
