943bfe44f6b8d57d1bc9e9ecbb62503ad1c0eab5
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-14 21:05:45 +0530

    added css

--- Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Navbar.js
+++ Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Navbar.js
@@ -3,23 +3,24 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <div className="w-9/12 mx-auto py-5 flex justify-between items-center">
+      <nav className="absolute right-0 left-0 z-10">
+        <div className="w-9/12 mx-auto py-5 flex justify-between items-center">
+          <div>
+            <button className="button py-2 px-4 rounded-md border text-purple-500 border-purple-500  bg-transparent hover:bg-purple-400 hover:text-white hover:border-white font-semibold">
+              Add Tasks
+            </button>
+          </div>
 
-        <div>
-          <button className="button py-2 px-4 rounded-md border text-purple-500 border-purple-500  bg-transparent hover:bg-purple-400 hover:text-white hover:border-white font-semibold">
-            Add Tasks
-          </button>
-        </div>
+          <div className="text-3xl font-Pin font-semibold">
+            <a href="#"> Doer</a>
+          </div>
 
-        <div className="text-3xl font-Pin font-semibold">
-          <a href="#"> Doer</a>
+          <div className="space-x-4">
+            <button className="text-lg ">Sign In</button>
+            <button className="text-lg ">Sign Up</button>
+          </div>
         </div>
-
-        <div className="space-x-4">
-          <button className="text-lg ">Sign In</button>
-          <button className="text-lg ">Sign Up</button>
-        </div>
-      </div>
+      </nav>
     </div>
   );
 }
