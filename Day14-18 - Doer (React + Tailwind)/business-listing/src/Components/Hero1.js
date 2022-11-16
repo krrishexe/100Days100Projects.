a2cb6954bb0a35be65068b44ac71adf8467e603e
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-16 14:00:25 +0530

    added shine effect

--- Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Hero1.js
+++ Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Hero1.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { DiCodepen } from "react-icons/fa";
-import { FaSadCry } from "react-icons/fa";
+import { FaCodepen } from "react-icons/fa";
 
 export default function Hero1(props) {
   return (
-    <div>
+    <>
       <div className="bg-white text-6xl py-48">
         <h1>
           <span>Tasks</span>
@@ -16,7 +16,7 @@ export default function Hero1(props) {
         </h1>
       </div>
 
-      <div className="relative h-100 heroBg -mb-36">
+      <div className="relative h-100 heroBg -mb-96">
         <div className="flex items-center justify-between ">
           <div className="flex flex-col text-start text-black ">
             <p className="text-7xl leading-tighter">
@@ -36,22 +36,31 @@ export default function Hero1(props) {
         <div className="bg bg2 text-6xl py-48"></div>
         <div className="bg bg3 text-6xl py-56"></div>
       </div>
-      <div className="-my-96 flex justify-center items-center flex-col">
-        <h1 className="font-rale ">Creator's <hr className=" my-5 bg-black/50 text-black" /> </h1>
+      <div className=" flex justify-center items-center flex-col">
+        <h1 className="font-rale ">
+          Creator's <hr className=" my-5 bg-black/50 text-black" />{" "}
+        </h1>
+      </div>
 
-        <div className="hero3container">
+        <div className=" flex items-center justify-evenly mt-10 ">
           <div className="card card0">
             <div className="hoverBorder">
               <h2>Al Pacino</h2>
             </div>
           </div>
-          <div className="card card1">
-            <div className="hoverBorder">
-              <h2>Ben Stiller</h2>
-            </div>
+          <div>
+          <p className=" text-7xl "><span className="effect-shine">Krish</span></p>
+          
           </div>
         </div>
-      </div>
-    </div>
+
+        {/* <div className="card card1">
+          <div className="hoverBorder">
+            <h2>Ben Stiller</h2>
+          </div>
+        </div>
+      </div> */}
+
+    </>
   );
 }
