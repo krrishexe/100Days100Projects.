42fd3c53b0d70ef7e4948b401297517a46697e44
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-16 10:50:21 +0530

    updated css

--- Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Hero1.js
+++ Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Hero1.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { FaHeart } from 'react-icons/fa';
+import { FaHeart } from "react-icons/fa";
 
 export default function Hero1(props) {
   return (
     <div>
-      <div className="bg-white text-6xl py-40">
+      <div className="bg-white text-6xl py-48">
         <h1>
           <span>Tasks</span>
           <div className="message font-rale">
@@ -16,15 +16,29 @@ export default function Hero1(props) {
       </div>
 
       <div className="relative h-100 heroBg">
-        <div className="flex items-center justify-between my-5">
-            <div className="flex flex-col text-start text-black ">
-                <p className="text-7xl leading-tighter">The only <span className=" text-black font-quick font-bold text-9xl">TASKS </span>
-                App You Will Ever Need. 🤍</p>
-                
-            </div>
+        <div className="flex items-center justify-between ">
+          <div className="flex flex-col text-start text-black ">
+            <p className="text-7xl leading-tighter">
+              The only
+              <span className=" text-black font-quick font-bold text-9xl">
+                TASKS 
+              </span>
+              App You Will Ever Need. 🤍
+            </p>
+          </div>
           <img src={props.hero} alt="" />
         </div>
-      </div> 
-    </div>  
+      </div>
+
+      <div className=" h-screen flex items-center justify-between">
+
+      <div class="bg text-6xl py-40"></div>
+      <div class="bg bg2 text-6xl py-48"></div>
+      <div class="bg bg3 text-6xl py-56"></div>
+      </div>
+      {/* <div class="content ">
+        <h1>Sliding Diagonals Background Effect</h1>
+      </div> */}
+    </div>
   );
 }
