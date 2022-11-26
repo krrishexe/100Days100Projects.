962a8538ed873235826cdacf300e4960d125a5a4
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-26 20:38:47 +0530

    updated sigin page

--- Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Signin.js
+++ Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Signin.js
@@ -1,4 +1,4 @@
-import React,{useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import noteContext from '../Context/notes/noteContext';
 import "../media/css/Signin.css";
 
@@ -12,22 +12,38 @@ const Signin = () => {
   // })
 
   return (
-    <div> 
-      <section className='signin'>
-      
-      </section>
+    <>
+    <div className='flex justify-center mt-28'>
+      <div className="main">
+        <input type="checkbox" id="chk" aria-hidden="true" />
 
+        <div className="signup">
+          <form>
+            <label htmlFor="chk" aria-hidden="true">Sign up</label>
+            <input type="text" name="txt" placeholder="User name" required="" />
+            <input type="email" name="email" placeholder="Email" required="" />
+            <input type="password" name="pswd" placeholder="Password" required="" />
+            <button>Sign up</button>
+          </form>
+        </div>
 
-
-
-
-
-
-
-      {/* <h1 className='relative text-4xl' >This is a Signin Component which is yet to be made</h1>
-      <p>hello {a.state.name} your reg no is {a.state.regno}</p> */}
-    </div>
+        <div className="login">
+          <form>
+            <label htmlFor="chk" aria-hidden="true">Login</label>
+            <input type="email" name="email" placeholder="Email" required="" />
+            <input type="password" name="pswd" placeholder="Password" required="" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+      </div>
+    </>
   )
 }
+
+/* <h1 className='relative text-4xl' >This is a Signin Component which is yet to be made</h1>
+<p>hello {a.state.name} your reg no is {a.state.regno}</p> */
+
+
 
 export default Signin;
