41b78ea52b96cbaba2158c1674e22c89d9166e0b
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-26 23:36:58 +0530

    added css

--- Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Signin.js
+++ Day14-18 - Doer (React + Tailwind)/business-listing/src/Components/Signin.js
@@ -20,10 +20,10 @@ const Signin = () => {
         <div className="signup">
           <form>
             <label htmlFor="chk" aria-hidden="true">Sign up</label>
-            <input type="text" name="txt" placeholder="User name" required="" />
+            {/* <input type="text" name="txt" placeholder="User name" required="" /> */}
             <input type="email" name="email" placeholder="Email" required="" />
             <input type="password" name="pswd" placeholder="Password" required="" />
-            <button>Sign up</button>
+            <button className='loginBtn '>Sign up</button>
           </form>
         </div>
 
@@ -32,7 +32,7 @@ const Signin = () => {
             <label htmlFor="chk" aria-hidden="true">Login</label>
             <input type="email" name="email" placeholder="Email" required="" />
             <input type="password" name="pswd" placeholder="Password" required="" />
-            <button>Login</button>
+            <button className='loginBtn'>Login</button>
           </form>
         </div>
       </div>
