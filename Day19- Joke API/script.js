f5c9a84754a5461168d106cb67316f3aa9c060a6
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-12-06 15:34:04 +0530

    added trycatch

--- Day19- Joke API/script.js
+++ Day19- Joke API/script.js
@@ -6,16 +6,21 @@ const Jokebtn = document.getElementById("btn");
 
 
 
-const generateJoke = async () =>{
+const generateJoke = () => {
 
-    url = "https://v2.jokeapi.dev/joke/Any"
-    fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        Joke.innerHTML = `<h1> ${json.setup} </h1>
+    try {
+        url = "https://v2.jokeapi.dev/joke/any"
+        fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                Joke.innerHTML = `<h1> ${json.setup} </h1>
                             <p> ${json.delivery} </p>`
-    })
+            })
 
-
-    console.log("joke generated");
+    } catch (error) {
+        console.log("error");
+    }
 }
+
+
+
