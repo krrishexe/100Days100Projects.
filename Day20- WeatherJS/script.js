f3d722a5ff334feec472e73f1a9a7050d5b802c1
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-12-18 11:22:30 +0530

    updated script.js

--- Day20- WeatherJS/script.js
+++ Day20- WeatherJS/script.js
@@ -1,11 +1,19 @@
-const weatherDiv = document.getElementById('weatherApi');
+const city = document.getElementById('city');
+const Title = document.getElementById('title');
+const Temp = document.getElementById('temp');
+const image = document.getElementById('imgid');
 
 
 const getnewweatherdata =  () =>{
-    fetch('http://api.weatherstack.com/current?access_key=548758f2ed6f39aca351ae6b81cfa6f3&query=New%20York')
+    fetch('http://api.weatherstack.com/current?access_key=548758f2ed6f39aca351ae6b81cfa6f3&query=jaipur')
     .then(resposne => resposne.json())
     .then(json => {
-        weatherDiv.innerText = `${json.location.name}`
+        Title.innerText = `${json.current.weather_descriptions}`
+        city.innerText = `${json.request.query}`
+        Temp.innerText = `${json.current.temperature}`
+        Temp.appendChild(document.createElement('img')).src = `${json.current.weather_icons}`;
+
+        // image.innerHTML = `<img id="imgid" src=${json.current.weather_icons}>`
         console.log("hello");
     } )
 
