683f5ba5edccb2572ef8cb7fcde27bfba5336dd2
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-12-20 14:32:01 +0530

    added post mmethod to the signup page

--- Day21-SignUp-Newsletter-front and back/app.js
+++ Day21-SignUp-Newsletter-front and back/app.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
+const request = require('request');
+const { urlencoded } = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static("public"))             // here we have to use the inbuilt static method of express which helps in uploading the static files of the website to the server .
 
 app.get("/",function(req,res){
-    res.send("Helllooo World");
+    res.sendFile(__dirname + "/Signup.html")
 })
 
+app.post("/",function(req,res){
+
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+
+    res.sendFile(__dirname + "/success.html")
+    console.log(fname + lname + email);
+
+
+})
 
 
 
