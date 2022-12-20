eb15edbacf5dfa18dc1d87f44917ef31aee0ebb3
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-12-20 11:29:38 +0530

    updated app.js and completed

--- Day20- WeatherJS/app.js
+++ Day20- WeatherJS/app.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const { response } = require("express");
 const express = require("express");
 const https = require('https');
@@ -5,14 +6,28 @@ const https = require('https');
 const app = express();
                                     // IKEA flatpak is unorganised data which can be in the form of hex code and we have to convert it into the json object using Parse method
 
+                                    // in order to read text or data ffrom the POST request , we need to use another module which is body-parser.
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",function(req,res){
 
-    const query = "paris"
+
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/",function(req,res){
+    
+    // console.log(req.body);            // req.body me vo sab data aajaega jo form ki body me hai.
+    
+    
+    const query = req.body.cityName;
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=07849c23723f8062fa611d36b059cba6`
 
     https.get(url,function(response){                       // same as fetching a url and then applying .then method
-        // console.log(response.statusCode);
+                                                            // console.log(response.statusCode);
 
         response.on("data",function(data){                          // syntax of https module
             const weatherData = JSON.parse(data)
@@ -20,16 +35,26 @@ app.get("/",function(req,res){
             const temp = weatherData.main.temp;
             const weatherDesc = weatherData.weather[0].description;
             const iconUrl = weatherData.weather[0].icon;
-            console.log(weatherDesc);
-            console.log(temp);
-            console.log(iconUrl);
+            // console.log(weatherDesc);
+            // console.log(temp);
+            // console.log(iconUrl);
             res.write(`<h1>weather quality is  :  ${weatherDesc}</h1>`)
-            res.write(`<h1>The temprature in jaipur is :  ${temp}</h1>`)
+            res.write(`<h1>The temprature in ${query} is :  ${temp}</h1>`)
             res.write(`<img src = "http://openweathermap.org/img/wn/${iconUrl}@2x.png" >`)
             res.send()
+
+            // we can do res.send() only one time , so what if we want to send more than one line of data to the user , then we can use the res.write() method.
+
+
         })
     })
+
+
+    console.log("post request recieved");
 })
+
+    
+
 
 
 
